85fbb0b57d365de0555c58139b6449077d373d36
elena-grozav <{elenagrozav@yahoo.com}>
2023-10-19 09:04:50 -0400

    Added badge for GitHub Actions

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -63,7 +63,7 @@ class TestAccountService(TestCase):
             account = AccountFactory()
             response = self.client.post(BASE_URL, json=account.serialize())
             self.assertEqual(
-                
+
                 response.status_code,
                 status.HTTP_201_CREATED,
                 "Could not create test Account",
@@ -147,7 +147,7 @@ class TestAccountService(TestCase):
         resp = self.client.get(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
-        self.assertEqual(len(data), 5)    
+        self.assertEqual(len(data), 5)
 
     def test_update_account(self):
         """It should Update an existing Account"""
@@ -162,11 +162,10 @@ class TestAccountService(TestCase):
         resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         updated_account = resp.get_json()
-        self.assertEqual(updated_account["name"], "Something Known")       
+        self.assertEqual(updated_account["name"], "Something Known")
 
     def test_delete_account(self):
         """It should Delete an Account"""
         account = self._create_accounts(1)[0]
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
-        
